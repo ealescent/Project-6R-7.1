5cc04de3ba17377fe98ed1691b104b8987c14ebe
ealescent <eal506@nyu.edu>
2020-10-27 20:56:56 -0400

    Finished?

--- sketch.js
+++ sketch.js
@@ -1,33 +1,3 @@
-let ball = []
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  ball0 = {
-    x: random(50),
-    y: random(height),
-    dx: 0,
-  }
-  ball1 = {
-    x: random(50),
-    y: random(height),
-    dx: 0,
-  }
-}
-function draw() {
-  background(200);
-  ball0.x += ball0.dx;
-  ball0.dx += 0.20;
-  circle(ball0.x, ball0.y, 50);
-  ball1.x += ball1.dx;
-  ball1.dx += 0.15;
-  circle(ball1.x, ball1.y, 50);
-  if (ball0.x >= width) {
-    ball0.dx = -0.70 * ball0.dx;
-  }
-  if (ball1.x >= width) {
-    ball1.dx = -0.95 * ball1.dx;
-  }
-}
-
 class Ball {
   constructor(x, y, dx, dy) {
     this.x = x;
@@ -35,12 +5,25 @@ class Ball {
     this.dx = dx;
     this.dy = dy;
   }
-  // containsPoint(x, y) {}
-  // containsMouse() {}
-  update() {
-    // changes the properties
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  ball0 = new Ball(random(50), random(height), 1);
+  ball1 = new Ball(random(50), random(height), 1);
+}
+function draw() {
+  background(200);
+  ball0.x += ball0.dx;
+  ball0.dx += 0.60;
+  circle(ball0.x, ball0.y, 50);
+  ball1.x += ball1.dx;
+  ball1.dx += 0.6;
+  circle(ball1.x, ball1.y, 50);
+  if (ball0.x >= width) {
+    ball0.dx = -0.75 * ball0.dx;
   }
-  draw() {
-    // renders the object
+  if (ball1.x >= width) {
+    ball1.dx = -.75 * ball1.dx;
   }
 }
